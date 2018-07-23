3bd77f703b446271983cfc7ccccae6b0147ac3c4
Ilyas Kabirov <ilyas.kabirov@gmail.com>
2018-07-23 15:18:52 +0300

    initial commit

---
+++ gulpfile.js
@@ -0,0 +1,19 @@
+const gulp = require('gulp');
+const modify = require('gulp-modify');
+
+function replaceContentsSync(file, content) {
+	return content.replace(/(@type|@param|@return)(.*?)(\})/g, (match, prefix, value, postfix) => {
+		value = value.replace(/([^!?a-zA-Z0-9.:](?:\.\.\.)?)([A-Z][a-z])/g, '$1?$2')
+		return `${prefix}${value}${postfix}`;
+	})
+}
+
+gulp.task("default", () => {
+	const dir = '.';
+	const filesMask = `${dir}\\**\\*.js`;
+	return gulp.src(filesMask)
+		.pipe(modify({
+			fileModifier: replaceContentsSync,
+		}))
+		.pipe(gulp.dest(dir));
+});
