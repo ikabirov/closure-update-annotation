df944a9ee9b4979d1d6a079f60ff167fa1dc47be
Ilyas Kabirov <ilyas.kabirov@gmail.com>
2018-07-23 16:45:05 +0300

    small fixes

--- gulpfile.js
+++ gulpfile.js
@@ -2,8 +2,8 @@ const gulp = require('gulp');
 const modify = require('gulp-modify');
 
 function replaceContentsSync(file, content) {
-	return content.replace(/(@type|@param|@return)(.*?)(\})/g, (match, prefix, value, postfix) => {
-		value = value.replace(/([^!?a-zA-Z0-9.:](?:\.\.\.)?)([A-Z][a-z])/g, '$1?$2')
+	return content.replace(/(@type|@param|@return)([^?]*?)(\})/g, (match, prefix, value, postfix) => {
+		value = value.replace(/([^!?a-zA-Z0-9.:_ ]\s*(?:\.\.\.)?)((?:[a-z]+[a-z.]*\.)?[A-Z][a-z])/g, '$1?$2');
 		return `${prefix}${value}${postfix}`;
 	})
 }
