4fc7598735840b3298d6cc3ba1a2002ac5935eb8
Ilyas Kabirov <ilyas.kabirov@gmail.com>
2018-08-07 11:43:28 +0300

    interface precessing was fixed

--- gulpfile.js
+++ gulpfile.js
@@ -3,14 +3,16 @@ const modify = require('gulp-modify');
 
 function replaceContentsSync(file, content) {
 	return content.replace(/(@type|@param|@return)([^?]*?)(\})/g, (match, prefix, value, postfix) => {
-		value = value.replace(/([^!?a-zA-Z0-9.:_ ]\s*(?:\.\.\.)?)((?:[a-z]+[a-z.]*\.)?[A-Z][a-z])/g, '$1?$2');
+		value = value.replace(/([^!?a-zA-Z0-9.:_ ]\s*(?:\.\.\.)?)((?:[a-z]+[a-z.]*\.)?[A-Z]+[a-z])/g, '$1?$2');
 		return `${prefix}${value}${postfix}`;
 	})
 }
 
 gulp.task("default", () => {
-	const dir = '.';
-	const filesMask = `${dir}\\**\\*.js`;
+	//const dir = 'c:\\!git\\flashspring\\flashspring\\htmlprojects\\libs\\closure\\closure';
+	const dir = 'C:/!git/flashspring/flashspring/htmlprojects/src';
+	//const filesMask = `${dir}\\**\\*.js`;
+	const filesMask = `${dir}\\**\\TooltipManager.js`;
 	return gulp.src(filesMask)
 		.pipe(modify({
 			fileModifier: replaceContentsSync,
